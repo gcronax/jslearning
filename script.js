3e03ccc61d0fabd7c0cd794a5ef74c54a04e1612
gcronax <146924808+gcronax@users.noreply.github.com>
2024-11-22 16:53:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+console.log('hola mundo'); 
+
+let x = 2; 
+
+ 
+ 
+
+let nombre = 'Pablo'; 
+
+ 
+ 
+
+let numeroentero=9; 
+
+let ndecimal=9.5; 
+
+let puntuacion=99; 
+
+let tienemascota=true; 
+
+let unavariable; 
+
+let nulo=null; 
+
+ 
+ 
+ 
+
+console.log(nombre); 
+
+console.log(puntuacion); 
+
+console.log(tienemascota); 
+
+console.log(unavariable); 
+
+console.log(nulo); 
+
+ 
+ 
+
+const numPI =3.1416; 
+
+console.log(numPI) 
+
+ 
+ 
+ 
+
+let edad =18; 
+
+let nombra = "pepe"; 
+
+console.log("hola soy +"+nombra+", tengo"+edad) 
+
+ 
+ 
+ 
+ 
+ 
+
+alert("bienbenino"); 
+
+var numero = prompt("ayuda"); 
+
+alert(numero+2); 
+
+numero= parseInt(numero); 
+
+alert(numero+2); 
+
+ 
+ 
+ 
+
+for (let index = 0; index < 10; index++) { 
+
+console.log(index) 
+
+} 
+
+let numeros= [1,2,3,4,5,6,7,8,9]; 
+
+for (let index = 0; index < numeros.length; index++) { 
+
+const element = numeros[index]; 
+
+console.log(element+"esto viene de una rray") 
+
+} 
