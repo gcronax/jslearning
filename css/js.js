11b77abb7f265bf36a3472e3c4258d76fb5dba44
gcronax <guillecampher15@gmail.com>
2025-01-15 19:24:39 +0100

    rwerwer

--- css/js.js
+++ css/js.js
@@ -1,3 +1,9 @@
-function aplicarEstiloclaro(){
-    document.getElementsByTagName('h1')[0].style.background="red";
+function aplicarEstiloclaro1(){
+    document.getElementsByTagName('h1')[0].style.background="white";
+}
+function aplicarEstiloclaro2(){
+    document.getElementsByTagName('h1')[0].style.background="black";
+}
+function aplicarEstiloclaro3(){
+    document.getElementsByTagName('h1')[0].style.background="black";
 }
